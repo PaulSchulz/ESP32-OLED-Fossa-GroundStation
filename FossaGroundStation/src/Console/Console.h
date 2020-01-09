9d985eb993fbe8fdd92697060c6e8a369ce1d367
Paul Schulz <paul@mawsonlakes.org>
2020-01-09 22:48:48 +1030

    Add initial template files
    Project compiles in PlatformIO

---
+++ FossaGroundStation/src/Console/Console.h
@@ -0,0 +1,37 @@
+/*
+  Console.h - Class to handle serial console
+  
+  Copyright (C) 2020 @Paul_Schulz
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _FOSSA_CONSOLE_H
+#define _FOSSA_CONSOLE_H
+
+#include "../ConfigManager/ConfigManager.h"
+#include "../Status.h"
+
+extern Status status;
+
+class Console {
+public:
+  Console(ConfigManager& x);
+  void init();
+
+private:
+  ConfigManager& configManager;
+};
+
+#endif
